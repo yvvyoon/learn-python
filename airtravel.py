2c0b03dc177066b057835e24b2fa8e2f6ae43b0c
yvvyoon <yoon4480@naver.com>
2019-12-30 01:02:38 +0900

    Modify: About class

--- airtravel.py
+++ airtravel.py
@@ -14,7 +14,51 @@
 #         return self._number
 
 
-class ClassMethod:
-    @classmethod
-    def print_name(cls):
-        print('My name is %s' % cls.__class__.__name__)
+# class ClassMethod:
+#     @classmethod
+#     def print_name(cls):
+#         print('My name is %s' % cls.__class__.__name__)
+#
+
+'''
+class Flight:
+    class_attr = []
+
+    def __init__(self):
+        self.class_attr = []
+
+    def add_class_attr(self, number):
+        Flight.class_attr.append(number)
+
+    def add_instance_attr(self, number):
+        self.class_attr.append(number)
+
+
+first = Flight()
+second = Flight()
+
+first.add_class_attr(5)
+print(first.class_attr)
+print(Flight.class_attr)
+print(second.class_attr)
+'''
+
+class Football:
+  rule = []
+
+
+class Baseball:
+  rule = []
+
+
+class Basketball:
+  rule = []
+
+
+class Sports(Football, Baseball, Basketball):
+  print(Football.rule)
+  print(Baseball.rule)
+  print(Basketball.rule)
+
+
+print(Sports.mro())
