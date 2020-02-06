538be74ba4b31e23f2e90bbd9d8bff5b57b415e6
yvvyoon <yoon4480@naver.com>
2020-02-06 14:06:54 +0900

    Add test files for multiprocessing

---
+++ multi_pro1.py
@@ -0,0 +1,14 @@
+import time
+
+start_time = time.time()
+
+def count(name):
+    for i in range(1, 200001):
+        print(f'{name}: {i}')
+
+num_list = ['p1', 'p2', 'p3', 'p4']
+
+for num in num_list:
+    count(num)
+
+print(f'*** {time.time() - start_time} ***')
---
+++ multi_pro2.py
@@ -0,0 +1,18 @@
+import time
+import multiprocessing
+
+start_time = time.time()
+
+def count(name):
+    for i in range(1, 200001):
+        print(f'{name}: {i}')
+
+num_list = ['p1', 'p2', 'p3', 'p4']
+
+if __name__ == '__main__':
+    pool = multiprocessing.Pool(processes=2)
+    pool.map(count, num_list)
+    pool.close()
+    pool.join()
+
+print(f'*** {time.time() - start_time} ***')
