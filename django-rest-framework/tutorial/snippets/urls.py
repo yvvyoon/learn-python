59e3fd8f7e10cc318bb267d331b2229b75a7f421
yvvyoon <yoon4480@naver.com>
2020-03-22 17:42:38 +0900

    Add format suffixes

--- django-rest-framework/tutorial/snippets/urls.py
+++ django-rest-framework/tutorial/snippets/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from snippets import views
 
@@ -7,3 +8,5 @@ urlpatterns = [
     path('snippets/', views.snippet_list),
     path('snippets/<int:pk>/', views.snippet_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- django-rest-framework/tutorial/snippets/views.py
+++ django-rest-framework/tutorial/snippets/views.py
@@ -7,12 +7,12 @@ from snippets.serializers import SnippetSerializer
 
 
 @api_view(['GET', 'POST'])
-def snippet_list(request):
+def snippet_list(request, format=None):
     if request.method == 'GET':
         snippets = Snippet.objects.all()
         serializer = SnippetSerializer(snippets, many=True)
 
-        return Response(serializer.data, safe=False)
+        return Response(serializer.data)
     elif request.method == 'POST':
         serializer = SnippetSerializer(data=request.data)
 
@@ -25,7 +25,7 @@ def snippet_list(request):
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
+def snippet_detail(request, pk, format=None):
     try:
         snippet = Snippet.objects.get(pk=pk)
     except Snippet.DoesNotExist:
