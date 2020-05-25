d19988190b9df789dabd4096ee803327897ec0ee
yvvyoon <yoon4480@naver.com>
2020-05-25 14:32:32 +0900

    Modify: Refactoring using Generic Views

--- django-rest-framework/tutorial/snippets/views.py
+++ django-rest-framework/tutorial/snippets/views.py
@@ -1,37 +1,14 @@
-from rest_framework import status
-from rest_framework import mixins
 from rest_framework import generics
-from django.http import Http404
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
 
-class SnippetList(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
-                  generics.GenericAPIView):
+class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class SnippetDetail(mixins.RetrieveModelMixin,
-                    mixins.UpdateModelMixin,
-                    mixins.DestroyModelMixin,
-                    generics.GenericAPIView):
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-
-    def get(self, request, *args, **kwargs):
-            return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-            return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-            return self.destroy(request, *args, **kwargs)
