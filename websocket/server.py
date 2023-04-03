f80149cef6116a1a2d285bb0129d80db067f591d
yvvyoon <yoon4480@naver.com>
2023-04-03 15:56:11 +0900

    Add: 웹 소켓 서버, 클라 예제

---
+++ websocket/server.py
@@ -0,0 +1,32 @@
+import asyncio
+import websockets
+
+
+entered_clients = set()
+
+
+async def echo(websocket):
+    print(f'websocket: {websocket}')
+    print(f'type of websocket: {type(websocket)}')
+
+    entered_clients.add(websocket)
+
+    try:
+        async for message in websocket:
+            print(f'Received message: {message}')
+            for client in entered_clients:
+                if client != websocket:
+                    await websocket.send(f'         ㄴㅡㅡ Echo reply: message')
+    except websocket.exception.ConnectionClosed as error:
+        print(f'Error: {error}')
+    finally:
+        entered_clients.remove(websocket)
+
+
+async def main():
+    server = await websockets.serve(echo, 'localhost', 8765)
+
+    await server.wait_closed()
+
+
+asyncio.run(main())
